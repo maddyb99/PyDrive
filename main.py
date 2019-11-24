72251f13c521a10d58a9eef529fb8d48d3dcdd02
maddyb99 <bavejam99@gmail.com>
2019-11-24 16:46:36 +0530

    some

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ import numpy as np
 import cv2
 from pykeyboard import PyKeyboard
 import threading
+import sys
+import os
 
 
 def edge_trace(orig):
     normalized=np.zeros((600,800))
-    hsv=np.zeros((600,800))
     gray=np.zeros((600,800))
-    hsv=cv2.cvtColor(orig,cv2.COLOR_BGR2HSV)
     normalized=cv2.normalize(orig,normalized,1,255,cv2.NORM_MINMAX)
     gray=cv2.cvtColor(normalized,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('hsv',hsv)
-    cv2.imshow('normal',normalized)
-    cv2.imshow('gray',gray)
+    # cv2.imshow('norm',cv2.Canny(normalized, threshold1=300, threshold2=50))
+    cv2.imshow('gray',cv2.Canny(gray, threshold1=300, threshold2=50))
     return cv2.Canny(gray, threshold1=300, threshold2=50)
 
 
@@ -73,10 +72,46 @@ def show_fb():
 
     cv2.destroyAllWindows()
 
+def test_fb(argv):
+    # sct = mss.mss()
+    pyk = PyKeyboard()
+    for image in argv:
+        # monitor = {'top': 100, 'left': 100, 'width': 800, 'height': 600}
+        img = cv2.imread(image)
+        img_np = np.array(img)
+        frame = cv2.resize(
+            img_np,
+            (int(img_np.shape[1]), int(img_np.shape[0])),
+        )
+        final=edge_trace(frame)
+        # cv2.imshow("FrameBuffer", final)
+        ip_key = cv2.waitKey(1)
+        if ip_key == 27 or ip_key == 113:
+            break
+        if ip_key == 97:
+            thread = threading.Thread(target=hold_key, args=(pyk, 'a', 0.5, 1.5, True))
+            thread.start()
+        if ip_key == 114:
+            release_all_keys(pyk)
+        image=image.replace("./img/","")
+        # print(os.path.join("./edge/edge_",image))
+        cv2.imwrite("./edge/"+image,final)
+
+    cv2.destroyAllWindows()
+    print("done")
+
 
 def main():
-    show_fb()
+    # print(sys.argv)
+    print(len(sys.argv))
+    # list=[]
+    if(len(sys.argv)==1):
+        show_fb()
+    else:
+        test_fb(sys.argv[1:])
 
 
 if __name__ == '__main__':
     main()
+
+#sudo systemctl disable gdm
